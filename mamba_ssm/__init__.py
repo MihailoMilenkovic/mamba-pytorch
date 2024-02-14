e672fb68c1b911c53a4e48fb8d90f55af3db9a64
Mihailo Milenkovic <mihailo.milenkovic4@gmail.com>
2024-02-14 02:34:16 -0800

    add selective scan fn to model file

--- mamba_ssm/__init__.py
+++ mamba_ssm/__init__.py
@@ -1,4 +1,3 @@
 __version__ = "1.1.1"
 
-from mamba_ssm.selective_scan_interface import selective_scan
 from mamba_ssm.model import Mamba, MambaLMHeadModel
--- mamba_ssm/model.py
+++ mamba_ssm/model.py
@@ -22,8 +22,6 @@ from collections import namedtuple
 
 from mamba_ssm.generation import GenerationMixin
 from mamba_ssm.hf import load_config_hf, load_state_dict_hf
-from mamba_ssm.selective_scan_interface import selective_scan
-
 
 
 @dataclass
@@ -93,6 +91,50 @@ class RMSNorm(torch.nn.Module):
             # residual_in_fp32=residual_in_fp32,
             upcast=True
         )
+
+
+def selective_scan(u, delta, A, B, C, D=None, z=None):
+    """
+    u: r(B D L)
+    delta: r(B D L)
+    A: c(D N) or r(D N)
+    B: c(D N) or r(B N L) or r(B N 2L) or r(B G N L) or (B G N L)
+    C: c(D N) or r(B N L) or r(B N 2L) or r(B G N L) or (B G N L)
+    D: r(D)
+    z: r(B D L)
+    delta_bias: r(D), fp32
+
+    out: r(B D L)
+    last_state (optional): r(B D dstate) or c(B D dstate)
+    """
+    dtype_in = u.dtype
+    u = u.float()
+    B = B.float()
+    C = C.float()
+    delta = delta.float()
+
+    batch, seqlen, dim, dstate = u.shape[0], u.shape[2], A.shape[0], A.shape[1]
+
+    x = A.new_zeros((batch, dim, dstate))
+
+    deltaA = torch.exp(torch.einsum('bdl,dn->bdln', delta, A))
+    deltaB_u = torch.einsum('bdl,bnl,bdl->bdln', delta, B, u)
+
+    ys = []
+    for i in range(seqlen):
+        x = deltaA[:, :, i] * x + deltaB_u[:, :, i]
+        y = torch.einsum('bdn,bn->bd', x, C[:, :, i])
+        #if y.is_complex():
+        #    y = y.real * 2
+        ys.append(y)
+    y = torch.stack(ys, dim=2) # (batch dim L)
+
+    out = y + u * rearrange(D, "d -> d 1")
+    if z is not None:
+        out = out * F.silu(z)
+    out = out.to(dtype=dtype_in)
+    return (out, x)
+
 
 def create_block(
     d_model,
